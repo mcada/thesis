29f382b111c09fd401646c8f550b8ae15822490b
Michael Cada <mcada@localhost.localdomain>
2018-10-16 20:05:03 +0200

    add frontend structure

---
+++ README.md
@@ -0,0 +1,19 @@
+THESIS IN PROGRESS
+
+
+todo list:
+
+##validace metod kontroleru
+    - update neresi jestli neco udelal nebo nebo ne
+    - zajistit aby sly provadet jen safe operace
+        - ACID
+
+##dodelat potrebne metody pro kontrolery
+    get employee tasks in date between <a,b>
+    pridani/update tasku musi vyvolat prepocitani bodu v review pro dane obdobi
+
+##zjistit jestli potrebuji routu '/'
+
+##refactorovat startovani expressu a pripojovani k databazi aby se to dalo
+    jednoduse parametrizovat napr. z openshift template
+
---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ListComponent } from './components/list/list.component';
+import { EditComponent } from './components/edit/edit.component';
+import { CreateComponent } from './components/create/create.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListComponent,
+    EditComponent,
+    CreateComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
