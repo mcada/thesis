79d10bafdbdd5d7933798e2999357b3590d07962
Michael Cada <mcada@redhat.com>
2018-10-17 21:50:23 +0200

    [frontend] install routing and angular-material

--- README.md
+++ README.md
@@ -3,17 +3,24 @@ THESIS IN PROGRESS
 
 todo list:
 
-##validace metod kontroleru
+#### validace metod kontroleru
     - update neresi jestli neco udelal nebo nebo ne
     - zajistit aby sly provadet jen safe operace
         - ACID
 
-##dodelat potrebne metody pro kontrolery
+#### dodelat potrebne metody pro kontrolery
     get employee tasks in date between <a,b>
     pridani/update tasku musi vyvolat prepocitani bodu v review pro dane obdobi
 
-##zjistit jestli potrebuji routu '/'
+#### zjistit jestli potrebuji routu '/'
 
-##refactorovat startovani expressu a pripojovani k databazi aby se to dalo
+#### refactorovat startovani expressu a pripojovani k databazi aby se to dalo
     jednoduse parametrizovat napr. z openshift template
 
+# frontend
+znovu nainstalovat prazdny project/quickstart + app-router + angular material module
+    - posefovat na netu jak spravne importovat angular material
+
+zjistit jak spravne nainstalovat service ktery pouziva httpclienta a vola
+express nodejs server 
+    - jak mit vice takovych servisu a parametrizovat URL
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,21 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ListComponent } from './components/list/list.component';
-import { EditComponent } from './components/edit/edit.component';
 import { CreateComponent } from './components/create/create.component';
+import { EditComponent } from './components/edit/edit.component';
+import { ListComponent } from './components/list/list.component';
+
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { MaterialModule } from './material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListComponent,
+    CreateComponent,
     EditComponent,
-    CreateComponent
+    ListComponent
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpModule,
+    MaterialModule,
     BrowserAnimationsModule
   ],
   providers: [],
