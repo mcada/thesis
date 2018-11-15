44408280147b4380ab6340342fc6faac017b5ed4
Michael Cada <mcada@redhat.com>
2018-11-15 21:14:45 +0100

    [frontend] changing current period now correctly changes current review

--- frontend/src/app/components/toolbar/toolbar.component.ts
+++ frontend/src/app/components/toolbar/toolbar.component.ts
@@ -37,18 +37,26 @@ export class ToolbarComponent implements OnInit, OnDestroy {
   }
 
   setConfig(config: Config) {
+    //update current period
     this.store.dispatch(new StateActions.ChangeConfig(config))
+
+    //update review list
     this.sub.add(this.reviewService.loadReviews(config._id)
       .subscribe(data => {
         this.store.dispatch(new StateActions.ChangeReviews(data))
       }))
 
     this.sub.add(this.state.subscribe(curr => {
+      //update task list
       this.sub.add(this.taskService.getTasks(curr.employee._id, curr.period.date_from, curr.period.date_to)
         .subscribe(tasks => {
           this.store.dispatch(new StateActions.ChangeTasks(tasks))
         }))
+      //update current review
+      this.sub.add(this.reviewService.getReviewById(curr.employee._id, curr.period._id)
+        .subscribe((review) => {
+          this.store.dispatch(new StateActions.ChangeReview(review))
+        }))
     }));
   }
-
 }
